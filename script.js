6a04e0cf20c1add5b9685c2ab1362fb5cae56c96
Daniela510 <whateverellie@gmail.com>
2022-04-20 23:17:01 +0200

    topics and start button

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
-const startMinutes = 10;
-let time = startMinutes * 60;
+const startMinutes = 2;
+let time; 
+let countActive = 0;
+let topics = [];
 
-const countdownP = document.getElementById('countdown');
-setInterval(updateCountdown, 1000)
-function updateCountdown(){
+const fetchTopics = async () => {
+  const response = await fetch("topics.json");
+  topics = await response.json();
+};
+fetchTopics();
 
-    const minutes = Math.floor(time/60);
+const startBtn = document.getElementById("start");
+const countdownP = document.getElementById("countdown");
+const topicContainer = document.getElementById("topic");
+
+const updateCountdown = () => {
+  //this conditional stops the setInterval from affecting the countdown before pressing start
+  if (countActive) {
+    const minutes = Math.floor(time / 60);
     let seconds = time % 60;
     seconds = seconds < 10 ? "0" + seconds : seconds;
     countdownP.innerHTML = `${minutes}:${seconds}`;
-    time--;
-}
+    if (time > 0) {
+      time--;
+    } else {
+      console.log(`${startMinutes} minutes passed`);
+      countActive = 0;
+    }
+  }
+};
+
+setInterval(updateCountdown, 1000);
+
+const startCountdown = () => {
+  time = startMinutes * 60;
+  countActive = 1;
+  let topic = topics[Math.floor(Math.random()*topics.length)];
+  topicContainer.innerHTML = topic;
+};
+
+startBtn.addEventListener("click", startCountdown);
