4a7e15aa519574c752a8372faad1469d14bf3594
Daniela510 <whateverellie@gmail.com>
2022-04-21 10:46:02 +0200

    functionality done

--- script.js
+++ script.js
@@ -2,16 +2,39 @@ const startMinutes = 2;
 let time; 
 let countActive = 0;
 let topics = [];
+let spices = [];
+let spiceIntervalID;
+let spiceTracker = 1;
 
 const fetchTopics = async () => {
   const response = await fetch("topics.json");
   topics = await response.json();
 };
 fetchTopics();
+const fetchSpices = async () => {
+  const response = await fetch("spice.json");
+  spices = await response.json();
+};
+fetchSpices();
 
 const startBtn = document.getElementById("start");
 const countdownP = document.getElementById("countdown");
 const topicContainer = document.getElementById("topic");
+
+const addSpice = () => {
+  if (spiceTracker < 5) {
+    const wordConainer = document.getElementById(`w${spiceTracker}`);
+    let spice = spices[Math.floor(Math.random()*spices.length)];
+    wordConainer.innerHTML ="- " + spice;
+    spiceTracker++;
+    console.log('active interval, next is: ', spiceTracker);
+  } else {
+    console.log('interval over');
+    clearInterval(spiceIntervalID);
+    spiceIntervalID = null;
+    spiceTracker = 1;
+  }
+  };
 
 const updateCountdown = () => {
   //this conditional stops the setInterval from affecting the countdown before pressing start
@@ -31,11 +54,13 @@ const updateCountdown = () => {
 
 setInterval(updateCountdown, 1000);
 
+
 const startCountdown = () => {
   time = startMinutes * 60;
   countActive = 1;
   let topic = topics[Math.floor(Math.random()*topics.length)];
   topicContainer.innerHTML = topic;
+  spiceIntervalID = setInterval(addSpice, 25000);
 };
 
 startBtn.addEventListener("click", startCountdown);
